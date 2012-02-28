e40b9ed0b57ba974cb3c4925cd8737ff87b864fd
Michael Barriault <mikebarriault@me.com>
2012-02-28 11:22:50 -0330

    Initial Commit

---
+++ DAccL/DAccL.h
@@ -0,0 +1,13 @@
+//
+//  DAccL.h
+//  DAccL
+//
+//  Created by Mike Barriault on 12-02-28.
+//  Copyright (c) 2012 Memorial University. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DAccL : NSObject
+
+@end
---
+++ DAccL/DAccL.m
@@ -0,0 +1,13 @@
+//
+//  DAccL.m
+//  DAccL
+//
+//  Created by Mike Barriault on 12-02-28.
+//  Copyright (c) 2012 Memorial University. All rights reserved.
+//
+
+#import "DAccL.h"
+
+@implementation DAccL
+
+@end
